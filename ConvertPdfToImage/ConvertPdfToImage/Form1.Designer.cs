d7017145051947c7a50bb9419abf2f55ebec284a
Rodwylson Lisbão <rodwylson@hotmail.com>
2018-07-20 00:20:15 -0300

    Criação do projeto, convert apenas as imagens que estão no pdf

---
+++ ConvertPdfToImage/ConvertPdfToImage/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ConvertPdfToImage
+{
+    partial class ConvertPdfToImageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPathFile = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtPathFile);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(495, 106);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Export";
+            // 
+            // txtPathFile
+            // 
+            this.txtPathFile.Location = new System.Drawing.Point(89, 28);
+            this.txtPathFile.Name = "txtPathFile";
+            this.txtPathFile.Size = new System.Drawing.Size(221, 20);
+            this.txtPathFile.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select pdf file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Save Path:";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // ConvertPdfToImageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 142);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ConvertPdfToImageForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPathFile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ ConvertPdfToImage/ConvertPdfToImage/Form1.cs
@@ -0,0 +1,101 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.Advanced;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertPdfToImage
+{
+    public partial class ConvertPdfToImageForm : Form
+    {
+        public ConvertPdfToImageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = txtPathFile.Text;
+                string pathSave = folderBrowserDialog1.SelectedPath;
+
+                try
+                {
+                    PdfDocument document = PdfReader.Open(file);
+
+                    int imageCount = 0;
+                    // Iterate pages
+                    foreach (PdfPage page in document.Pages)
+                    {
+                        // Get resources dictionary
+                        PdfDictionary resources = page.Elements.GetDictionary("/Resources");
+
+
+                        if (resources != null)
+                        {
+                            // Get external objects dictionary
+                            PdfDictionary xObjects = resources.Elements.GetDictionary("/XObject");
+                            if (xObjects != null)
+                            {
+                                ICollection<PdfItem> items = xObjects.Elements.Values;
+                                // Iterate references to external objects
+                                foreach (PdfItem item in items)
+                                {
+                                    PdfReference reference = item as PdfReference;
+                                    if (reference != null)
+                                    {
+                                        PdfDictionary xObject = reference.Value as PdfDictionary;
+                                        // Is external object an image?
+                                        if (xObject != null && xObject.Elements.GetString("/Subtype") == "/Image")
+                                        {
+                                            ExportJpegImage(xObject, ref imageCount, pathSave);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    MessageBox.Show(imageCount + " images exported.", "Export Images");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        static void ExportJpegImage(PdfDictionary image, ref int count, string destinPath)
+        {
+            // Fortunately JPEG has native support in PDF and exporting an image is just writing the stream to a file.
+            byte[] stream = image.Stream.Value;
+            FileStream fs = new FileStream(Path.Combine(destinPath, "Image.tiff"), FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(stream);
+            bw.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = openFileDialog1.ShowDialog(); 
+
+
+            if (result == DialogResult.OK) 
+            {
+                txtPathFile.Text = openFileDialog1.FileName;
+
+            }
+        }
+    }
+}
